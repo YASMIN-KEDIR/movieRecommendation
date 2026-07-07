87b691d1787f88e2c68f559dfcc7f6fb5b9bb89a
yasmin kedir <yasmina.k1781@gmail.com>
2026-07-07 03:37:36 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper"> 
+ <header>
+  <h1>find <span claaName="text-gradient"> islamic kids</span> entertainment with knowledge</h1>
+ </header>
+ <p> Search</p>
+      </div>
+    </main>
+  )
+}
+
+export default App
